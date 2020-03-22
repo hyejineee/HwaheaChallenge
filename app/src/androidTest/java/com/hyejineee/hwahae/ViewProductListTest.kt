5731aee04c17211655d273c040451e9c81fd54ef
hyeonhyejin <dev.hyejin04@gmail.com>
2020-03-22 23:14:26 +0900

    Add a test to see a list of filtering product.

--- app/src/androidTest/java/com/hyejineee/hwahae/ViewProductListTest.kt
+++ app/src/androidTest/java/com/hyejineee/hwahae/ViewProductListTest.kt
@@ -1,6 +1,7 @@
 package com.hyejineee.hwahae
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.*
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.filters.LargeTest
@@ -21,5 +22,15 @@ class ViewProductListTest {
     fun viewDefaultProductList(){
         onView(withText("모든 피부 타입")).check(matches(isDisplayed()))
         onView(withText("플라멜엠디 밀크러스트필 마일드 워시오프 앰플 5ml x 2개")).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun viewFilteringProductList(){
+        onView(withId(R.id.skin_type_sp)).perform(click())
+
+        onView(withText("민감성")).check(matches(isDisplayed()))
+        onView(withText("민감성")).perform(click())
+
+        onView(withText("엔오에이치제이 코리안 에스테틱 마스크 포어버블")).check(matches(isDisplayed()))
     }
 }
